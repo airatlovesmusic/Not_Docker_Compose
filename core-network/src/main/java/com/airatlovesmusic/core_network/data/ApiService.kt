3c46be538dcee87f9b0935a5de5eb77a494f3464
airatlovesmusic <airat@rcsmail.ru>
2020-08-23 23:50:39 +0300

    Add NetworkModule.kt and ApiService.kt for Retrofit

---
+++ core-network/src/main/java/com/airatlovesmusic/core_network/data/ApiService.kt
@@ -0,0 +1,9 @@
+package com.airatlovesmusic.core_network.data
+
+/**
+ * Created by Airat Khalilov on 23/08/2020.
+ */
+
+interface ApiService {
+
+}
---
+++ core-network/src/main/java/com/airatlovesmusic/core_network/di/NetworkModule.kt
@@ -0,0 +1,13 @@
+package com.airatlovesmusic.core_network.di
+
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+/**
+ * Created by Airat Khalilov on 23/08/2020.
+ */
+
+@InstallIn(ApplicationContext::class)
+class NetworkModule {
+
+}
