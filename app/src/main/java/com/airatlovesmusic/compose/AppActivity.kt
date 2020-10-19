e961516472cf9d2f0e217de1c56879860685adfb
airatlovesmusic <airat@rcsmail.ru>
2020-10-19 10:39:45 +0300

    Refactoring

--- app/src/main/java/com/airatlovesmusic/compose/AppActivity.kt
+++ app/src/main/java/com/airatlovesmusic/compose/AppActivity.kt
@@ -3,18 +3,9 @@ package com.airatlovesmusic.compose
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.animation.Crossfade
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
 import com.airatlovesmusic.core_network.ApiClient
 import com.airatlovesmusic.core_network.ApiClientImpl
-import com.airatlovesmusic.articles.Articles
-import com.airatlovesmusic.article.Article
-import com.airatlovesmusic.core_common.Screens
-import org.koin.androidx.compose.viewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
